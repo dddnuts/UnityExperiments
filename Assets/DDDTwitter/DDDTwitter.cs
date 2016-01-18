4c7095d65b32a225f741e5aa3850a338aad3fc6c
dddnuts <dddnuts@kakeragames.com>
2016-01-19 01:13:29 +0900

    Post tweet

---
+++ Assets/DDDTwitter/DDDTwitter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DDDTwitter
+{
+    public void Post(string message)
+    {
+        var dddTwitter = new AndroidJavaObject("com.kakeragames.dddtwitter.DDDTwitter");
+        dddTwitter.Call("post", message);
+    }
+}
---
+++ Assets/DDDTwitter/Example/Scripts/TweetPoster.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TweetPoster : MonoBehaviour
+{
+    [SerializeField]
+    private InputField message;
+
+    private DDDTwitter twitter;
+
+    void Awake()
+    {
+        twitter = new DDDTwitter();
+    }
+
+    public void OnClickPost()
+    {
+        twitter.Post(message.text);
+    }
+}
