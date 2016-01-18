7b8cffacf84b45250064bae8f78ba278bc47b0ed
dddnuts <dddnuts@kakeragames.com>
2016-01-19 01:26:49 +0900

    Use using block

--- Assets/DDDTwitter/DDDTwitter.cs
+++ Assets/DDDTwitter/DDDTwitter.cs
@@ -4,7 +4,9 @@ public class DDDTwitter
 {
     public void Post(string message)
     {
-        var dddTwitter = new AndroidJavaObject("com.kakeragames.dddtwitter.DDDTwitter");
-        dddTwitter.Call("post", message);
+        using (var dddTwitter = new AndroidJavaObject("com.kakeragames.dddtwitter.DDDTwitter"))
+        {
+            dddTwitter.Call("post", message);
+        }
     }
 }
